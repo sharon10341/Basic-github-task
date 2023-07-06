37f9db5ae95a93c61c1b08fab2643adb4df9f224
Mercy <mercysharon850@gmail.com>
2023-07-06 17:15:03 +0100

    Javacommit

---
+++ FirstJavaProject.java
@@ -0,0 +1,5 @@
+public class FirstJavaProject {
+    public static void main(String[] args){
+        System.out.println("Hello My name is Mercy and i'm currently undergoing a 3 month training on software testing and this is my first  Hello Word,");
+    }
+}
